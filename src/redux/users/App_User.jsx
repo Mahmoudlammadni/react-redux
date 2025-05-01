5071bccae81eaacb8e5ddb82b6c69175d2201ac3
Mahmoud <lamm.mah2002@gmail.com>
2025-05-01 23:15:29 +0200

    fix the style of the buttoms in the home page of the users

--- src/redux/users/App_User.jsx
+++ src/redux/users/App_User.jsx
@@ -6,16 +6,15 @@ import { Link, Routes, Route } from "react-router-dom";
 export default function App_User() {
     return (
         <div className="container mt-5">
-            <nav className="mb-4">
-                <Link to="/u/" className="btn btn-primary">Home</Link>
-                <Link to="/u/a" className="btn btn-primary">Add</Link>
+            <nav className="mb-4 d-flex gap-3">
+                <Link to="/u/" className="btn btn-outline-primary px-4 fw-bold"> Home</Link>
+                <Link to="/u/a" className="btn btn-outline-success px-4 fw-bold">Add</Link>
             </nav>
-                <Routes>
-                    <Route path="/" element={<User />} />
-                    <Route path="/a" element={<Add_user />} />
-                    <Route path="/m/:id" element={<Update_Users/>} />
-                </Routes>
-          
+            <Routes>
+                <Route path="/" element={<User />} />
+                <Route path="/a" element={<Add_user />} />
+                <Route path="/m/:id" element={<Update_Users />} />
+            </Routes>
         </div>
     );
 }
