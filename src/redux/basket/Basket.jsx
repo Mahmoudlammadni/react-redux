74053367ffc7cada781052e934cfbe59cb2c594e
Mahmoud <lamm.mah2002@gmail.com>
2025-05-03 22:55:46 +0200

    display the data of the basket

--- src/redux/basket/Basket.jsx
+++ src/redux/basket/Basket.jsx
@@ -1,6 +1,24 @@
-export default function Basket()
-{
-   return(
-    <p>comming !!</p>
-   )
+import { useSelector } from "react-redux";
+
+export default function Basket() {
+  const basket = useSelector(state => state.basket);
+
+  return (
+    <div className="container mt-4">
+      <h2 className="text-center text-primary mb-4">🛒 Votre Panier</h2>
+      <div className="row">
+        {basket.map((item) => (
+          <div key={item.id} className="col-md-4 mb-3">
+            <div className="card shadow-sm border-0 rounded">
+              <div className="card-body">
+                <h5 className="card-title text-success">{item.nom}</h5>
+                <p className="card-text"><strong>Type :</strong> {item.type}</p>
+                <p className="card-text"><strong>Quantité :</strong> {item.quantite}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
